979c692d4c466e90903186b7f23b813e3e1702de
Adam Jahur <adamnjahur@gmail.com>
2017-03-31 14:27:12 -0400

    added listing for all payment

--- list_all_payments.php
+++ list_all_payments.php
@@ -20,8 +20,46 @@ catch(Exception $e) {
 
 ?>
 <style>
-.form-basic{
+	.form-basic{
 
-max-width: 741px !important;
-}
+		max-width: 741px !important;
+	}
 </style>
+<div class="main-content">
+
+
+
+	<form class="form-basic" method="post" action="">
+
+		<div class="form-title-row">
+			<h1>Payment Listing </h1>
+		</div>
+		
+		<?php
+		
+		echo "<table><tr>
+		<th>Charge id</th>
+		<th>Amount</th>
+		<th>Customer</th>
+		<th>Created</th>
+	</tr>";
+	if(!empty($list_array['data'])){
+		foreach($list_array['data'] as $detail){
+			
+			echo "<tr><td>".$detail['id']."</td><td>".$detail['amount']."</td><td>".$detail['customer']."</td><td>".date("Y-m-d H-i-s",$detail['created'])."</td></tr>";	
+		}
+	}
+	else{
+		
+		echo "<tr>Empty</tr>";			
+	}
+	echo "</table>";
+	
+	?>
+</form>
+
+</div>
+
+</body>
+
+</html>
